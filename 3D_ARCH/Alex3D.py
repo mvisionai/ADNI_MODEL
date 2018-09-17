04f3382e458e44a2ca4a72e2860ab58373bc3db9
mvisionai <vision2bernard2015@gmail.com>
2018-09-16 23:46:35 -0700

    tuned

--- 3D_ARCH/Alex3D.py
+++ 3D_ARCH/Alex3D.py
@@ -48,6 +48,7 @@ def inference(inputs, dropout_keep_prob, label_cnt):
             with tf.variable_scope('conv2layer'):
                 conv2 = op.conv(conv1, 5, 256, 1, 0.1)
                 conv2 = op.lrn(conv2)
+                        
                 conv2 = tf.nn.max_pool3d(conv2, ksize=[1, 2, 2, 2, 1], strides=[1, 1, 1, 1, 1], padding='VALID')
 
             # conv layer 3
